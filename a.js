31a050de523a0b9d3692eefd216790c0d63e5984
jewel <snowdecember079@gmail.com>
2023-11-05 17:12:11 -0500

    firstday

---
+++ a.js
@@ -0,0 +1,9 @@
+console.log("practice")
+
+// //1. Assign a new value to the variable num. The code will not work the way it is. Find the 
+// mistake and fix it. Execute the corrected code.//
+
+let num = 1;
+num = 2;
+console.log(num);
+
